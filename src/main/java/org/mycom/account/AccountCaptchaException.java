ba8093debbffa474f8fdac6fd0627e5deda99fd3
wangliyue <1058101876@qq.com>
2018-12-14 18:11:37 +0800

    captcha modify

---
+++ src/main/java/org/mycom/account/AccountCaptchaException.java
@@ -0,0 +1,17 @@
+package org.mycom.account;
+
+public class AccountCaptchaException
+    extends Exception
+{
+    private static final long serialVersionUID = 1339439433313285858L;
+
+    public AccountCaptchaException( String message )
+    {
+        super( message );
+    }
+
+    public AccountCaptchaException( String message, Throwable throwable )
+    {
+        super( message, throwable );
+    }
+}
